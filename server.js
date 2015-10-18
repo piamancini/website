ea64013bb73b5b79d158f148addff2a284fe9025
bleedingxedge <maru@bxe.me>
2015-10-17 20:28:12 -0500

    Finally

--- server.js
+++ server.js
@@ -9,30 +9,26 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.use(bodyParser.urlencoded({ extended: true })); // Parses application/x-www-form-urlencoded
+app.use(bodyParser.json())
+app.use( bodyParser.urlencoded( { extended: true } ) );
+
 
 var transporter = nodemailer.createTransport({
         service: 'Gmail',
         auth: {
-            user: 'maru@bxe.me',
-            pass: 'maruxmaruxmaru'
+            user: 'SENDEREMAIL',
+            pass: 'PASSWORD'
         }
     });
 
 app.post('/', function (req, res){
+
     console.log(req.body)
 
-    var email = req.body.email || 'hola@maruma.ru'
-
-    var mensaje  = req.body.mensaje || (req.body.email + ' Has requested to join the list on OpenCollective');
-    var subject = req.body.asunto || 'Mailing List Candidate';
-    if(req.body.mensaje){
-        mensaje += "<br>" +"<br>" +"---" +"<br>" +"<br>" + email + " has requested to join the mailing list and be a candidate for OpenCollective"
-    }
       // setup e-mail data with unicode symbols
       var mailOptions = {
-        from: 'maru@bxe.me', // sender address
-        to: 'hola@maruma.ru', // list of receivers
+        from: 'SENDEREMAIL', // sender address
+        to: 'RECEIVEREMAIL', // list of receivers
         subject: 'Mailing List Candidate', // Subject line
         text: req.body.email + ' Puchi', // plaintext body
         html: '<a mailto:'+req.body.email+'/>'+req.body.email+'</a> Has requested to join the list on OpenCollective' // html body
@@ -41,40 +37,44 @@ app.post('/', function (req, res){
     // send mail with defined transport object
     transporter.sendMail(mailOptions, function(error, info){
         if(error){
-            res.status(400, 'Not happening.')
-            res.send('Nope. This is a failure.')
-        } else {
-            res.status(200, 'Yay! Here goes!')
-            res.redirect('/')
+            res.sendStatus(400, 'Not happening.')
+            return res.send('Nope. This is a failure.')
         }
+        res.sendStatus(200, 'Yay! Here goes!')
     });
 
 })
 
-
-app.post('/followup', function (req, res){
-    console.log(req.body)
-
+app.post('/send',function(req, res){
+    var candidate = req.body.candidate
+    var textarea = req.body.textarea
+    var country = req.body.country
+    var account = req.body.onoffswitch
+    var select = req.body.select
     // setup e-mail data with unicode symbols
-      var mailOptions = {
-        from: 'maru@bxe.me', // sender address
-        to: 'hola@maruma.ru', // list of receivers
-        subject: 'Follow up', // Subject line
-        text: req.body, // plaintext body
-        html: req.body // html body
+
+    var options = {
+        from: 'SENDEREMAIL', // sender address
+        to: 'RECEIVEREMAIL', // list of receivers
+        subject: 'Follow up for Candidate ' + candidate, // Subject line
+        text: 'Follow up for Candidate' + candidate, // plaintext body
+        html: 'Candidate: <a mailto:' + candidate + '/>' + candidate + '</a><br/> Reason/Project: ' + textarea + '<br/> Country: ' + country + '<br/> Account: ' + account + '<br/> Expected Profit: ' + select // html body
     };
 
+    console.log(options)
+
     // send mail with defined transport object
-    transporter.sendMail(mailOptions, function(error, info){
+    transporter.sendMail(options, function(error, info){
         if(error){
-            res.status(400, 'Not happening.')
-            res.send('Nope. This is a failure.')
-        } else {
-            res.status(200, 'Yay! Here goes!')
-            res.redirect('/')
+            return res.sendStatus(400, 'Not happening.')
         }
+        console.log('ok')
+        res.sendStatus(200, 'Yay! Here goes!')
     });
-})
+
+});
+
+app.use(require('morgan')())
 
 app.use('/styles', express.static(__dirname + '/styles'));
 app.use('/assets', express.static(__dirname + '/assets'));
