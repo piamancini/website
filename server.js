27684670126b7a2ed727ae9eb452c21ef063b085
bleedingxedge <maru@bxe.me>
2015-10-26 16:41:46 -0600

    Testing changes

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ app.post('/send',function(req, res){
 app.use('/styles', express.static(__dirname + '/public/styles'))
 app.use('/assets', express.static(__dirname + '/public/assets'))
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address
   var port = server.address().port
 
